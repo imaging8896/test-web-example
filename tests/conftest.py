6a0d552fa14c5b059c9b1c8281780afc9048957f
Ted Chen <imaging8896@gmail.com>
2022-07-18 16:48:04 +0800

    [tests] Fix not capture screenshot when test failed

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,6 +1,6 @@
 import pytest
 
-from .driver import selenium_driver
+from .driver import selenium_driver, capture_screenshot_on_test_failed
 from .user import browser_user
 
 # set up a hook to be able to check if a test has failed
